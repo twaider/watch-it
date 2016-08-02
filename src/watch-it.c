01f6e7bac72ea592b7b3c9330ebe9409bb88cc73
Andrei Teodorescu <andrei@globalmouth.com>
2016-08-02 10:27:28 +0200

    Updates

--- src/watch-it.c
+++ src/watch-it.c
@@ -13,28 +13,42 @@ typedef struct {
 
 static Window *s_main_window;
 static Layer *s_canvas_layer;
-static TextLayer *s_weather_layer, *s_date_layer, *s_hour_layer,
-    *s_minute_layer, *s_icon_layer;
+static TextLayer *s_date_layer, *s_hour_layer, *s_minute_layer,
+    *s_icontext_layer, *s_icon_layer;
 
 static GFont s_small_font, s_time_font, s_icon_font;
 
 static GPoint s_center;
 static char s_last_hour[8], s_last_minute[8], s_last_date[16],
-    s_battery_icon[8], s_battery_text[16];
+    s_icon_content[8], s_icon_text[16];
 static int text1_color, text2_color;
 
 static bool weather_units_conf = false, weather_safemode_conf = true,
-            weather_status_conf = false,
-            connection_status = false, weather_on_conf = false,
-            text_color_on_conf = false;
+            weather_data_conf = false, connection_status = false,
+            weather_on_conf = false, text_color_on_conf = false;
 
-/*************************** appMessage **************************/
+// Set battery text and icon
+static void set_battery(int battery_level) {
+  if (battery_level / 10 <= 10 && battery_level / 10 > 7) {
+    snprintf(s_icon_content, sizeof(s_icon_content), "%s", "z");
+  } else if (battery_level / 10 <= 7 && battery_level / 10 > 3) {
+    snprintf(s_icon_content, sizeof(s_icon_content), "%s", "x");
+  } else if (battery_level / 10 <= 3 && battery_level / 10 > 0) {
+    snprintf(s_icon_content, sizeof(s_icon_content), "%s", "y");
+  }
+  snprintf(s_icon_text, sizeof(s_icon_text), "%d\n %%", battery_level);
+}
 
+// Battery handler
+static void battery_handler(BatteryChargeState state) {
+  if (!weather_on_conf || !weather_data_conf) {
+    set_battery(state.charge_percent);
+  }
+}
+
+// App messages
 static void inbox_received_callback(DictionaryIterator *iterator,
                                     void *context) {
-  // Store incoming information
-  static char icon_buffer[16];
-  static char temperature_buffer[8];
   static int temperature;
 
   // Read tuples for data
@@ -51,16 +65,12 @@ static void inbox_received_callback(DictionaryIterator *iterator,
 
   // If we get weather option
   if (weather_on_tuple) {
-    APP_LOG(APP_LOG_LEVEL_INFO, "weather_on_tuple");
-    // Set weather flag
     weather_on_conf = (bool)weather_on_tuple->value->int16;
     persist_write_bool(MESSAGE_KEY_WEATHER_ON, weather_on_conf);
   }
-  
+
   if (weather_status_tuple) {
-    APP_LOG(APP_LOG_LEVEL_INFO, "weather_on_tuple");
-    // Set weather flag
-    weather_status_conf = (bool)weather_status_tuple->value->int16;
+    weather_data_conf = (bool)weather_status_tuple->value->int16;
   }
 
   if (weather_safemode_tuple) {
@@ -75,31 +85,22 @@ static void inbox_received_callback(DictionaryIterator *iterator,
 
   // If all data is available, use it
   if (temp_tuple && icon_tuple) {
-    APP_LOG(APP_LOG_LEVEL_INFO, "temp_tuple");
+    APP_LOG(APP_LOG_LEVEL_INFO, "icon_tuple");
     // Assemble strings for temp and icon
     temperature = (float)temp_tuple->value->int32;
 
     if (weather_units_conf) {
-      snprintf(temperature_buffer, sizeof(temperature_buffer), "%d\n°F",
-               temperature);
+      snprintf(s_icon_text, sizeof(s_icon_text), "%d\n°F", temperature);
     } else {
-      snprintf(temperature_buffer, sizeof(temperature_buffer), "%d\n°C",
-               temperature);
+      snprintf(s_icon_text, sizeof(s_icon_text), "%d\n°C", temperature);
     }
 
-    snprintf(icon_buffer, sizeof(icon_buffer), "%s",
+    snprintf(s_icon_content, sizeof(s_icon_content), "%s",
              icon_tuple->value->cstring);
-
-    // Set temp and icon to text layers
-    text_layer_set_text(s_weather_layer, temperature_buffer);
-    text_layer_set_text(s_icon_layer, icon_buffer);
   }
 
-  // If weather disabled, clear weather layers
-  if (!weather_on_conf || !weather_status_conf) {
-    text_layer_set_text(s_weather_layer, s_battery_text);
-    text_layer_set_text(s_icon_layer, s_battery_icon);
-  }
+  // If weather disabled or no data display battery
+  battery_handler(battery_state_service_peek());
 
   // If text color and enabled
   if (text1_color_tuple && text2_color_tuple && text_color_on_tuple) {
@@ -134,8 +135,7 @@ static void outbox_sent_callback(DictionaryIterator *iterator, void *context) {
   APP_LOG(APP_LOG_LEVEL_INFO, "Outbox send success!");
 }
 
-/************************************ UI **************************************/
-
+// Tick handled
 static void tick_handler(struct tm *tick_time, TimeUnits changed) {
   static bool in_interval = true;
 
@@ -173,7 +173,6 @@ static void tick_handler(struct tm *tick_time, TimeUnits changed) {
 }
 
 static void update_proc(Layer *layer, GContext *ctx) {
-  // Color background?
   GRect bounds = layer_get_bounds(layer);
   GRect block = GRect(0, 110, bounds.size.w, 60);
 
@@ -186,16 +185,15 @@ static void update_proc(Layer *layer, GContext *ctx) {
   text_layer_set_text(s_hour_layer, s_last_hour);
   text_layer_set_text(s_minute_layer, s_last_minute);
 
-  if (!weather_on_conf || !weather_status_conf) {
-    text_layer_set_text(s_weather_layer, s_battery_text);
-    text_layer_set_text(s_icon_layer, s_battery_icon);
-  }
+  // Set icon and icon text layers
+  text_layer_set_text(s_icontext_layer, s_icon_text);
+  text_layer_set_text(s_icon_layer, s_icon_content);
 
   // If color screen set text color
   if (COLORS) {
     text_layer_set_text_color(s_hour_layer, GColorFromHEX(text1_color));
     text_layer_set_text_color(s_icon_layer, GColorFromHEX(text2_color));
-    text_layer_set_text_color(s_weather_layer, GColorFromHEX(text2_color));
+    text_layer_set_text_color(s_icontext_layer, GColorFromHEX(text2_color));
     text_layer_set_text_color(s_date_layer, GColorFromHEX(text2_color));
   }
 
@@ -244,23 +242,6 @@ static void update_proc(Layer *layer, GContext *ctx) {
   }
 }
 
-static void set_battery(int battery_level) {
-  if (battery_level / 10 <= 10 && battery_level / 10 > 7) {
-    snprintf(s_battery_icon, sizeof(s_battery_icon), "%s", "z");
-  } else if (battery_level / 10 <= 7 && battery_level / 10 > 3) {
-    snprintf(s_battery_icon, sizeof(s_battery_icon), "%s", "x");
-  } else if (battery_level / 10 <= 3 && battery_level / 10 > 0) {
-    snprintf(s_battery_icon, sizeof(s_battery_icon), "%s", "y");
-  }
-  snprintf(s_battery_text, sizeof(s_battery_text), "%d\n %%", battery_level);
-}
-
-static void battery_handler(BatteryChargeState state) {
-    set_battery(state.charge_percent);
-    text_layer_set_text(s_weather_layer, s_battery_text);
-    text_layer_set_text(s_icon_layer, s_battery_icon);
-}
-
 static void window_load(Window *window) {
   Layer *window_layer = window_get_root_layer(window);
   GRect window_bounds = layer_get_bounds(window_layer);
@@ -280,7 +261,7 @@ static void window_load(Window *window) {
 
   layer_add_child(window_layer, s_canvas_layer);
 
-  // Create time Layer
+  // Create time Layers
   s_hour_layer =
       text_layer_create(GRect(0, PBL_IF_ROUND_ELSE(0, 0),
                               window_bounds.size.w / 2, window_bounds.size.h));
@@ -299,30 +280,32 @@ static void window_load(Window *window) {
   text_layer_set_text_alignment(s_minute_layer, GTextAlignmentCenter);
 
   // Create date Layer
-  s_date_layer = text_layer_create(
-      GRect(window_bounds.size.w / 2,
-            PBL_IF_ROUND_ELSE(window_bounds.size.h, window_bounds.size.h - 51),
-            window_bounds.size.w / 2, 60));
+  s_date_layer = text_layer_create(GRect(
+      window_bounds.size.w / 2 + 12,
+      PBL_IF_ROUND_ELSE(window_bounds.size.h - 51, window_bounds.size.h - 51),
+      window_bounds.size.w / 2, 60));
 
   // Style the date text
   text_layer_set_background_color(s_date_layer, GColorClear);
   text_layer_set_text_color(s_date_layer, GColorWhite);
-  text_layer_set_text_alignment(s_date_layer, GTextAlignmentCenter);
+  text_layer_set_text_alignment(s_date_layer, GTextAlignmentLeft);
 
   // Create weather icon Layer
-  s_weather_layer = text_layer_create(GRect(
-      45, PBL_IF_ROUND_ELSE(window_bounds.size.h, window_bounds.size.h - 51),
-      window_bounds.size.w / 2, 60));
+  s_icontext_layer =
+      text_layer_create(GRect(45, PBL_IF_ROUND_ELSE(window_bounds.size.h - 51,
+                                                    window_bounds.size.h - 51),
+                              window_bounds.size.w / 2, 60));
 
   // Style the weather
-  text_layer_set_background_color(s_weather_layer, GColorClear);
-  text_layer_set_text_color(s_weather_layer, GColorWhite);
-  text_layer_set_text_alignment(s_weather_layer, GTextAlignmentLeft);
+  text_layer_set_background_color(s_icontext_layer, GColorClear);
+  text_layer_set_text_color(s_icontext_layer, GColorWhite);
+  text_layer_set_text_alignment(s_icontext_layer, GTextAlignmentLeft);
 
   // Create weather icon Layer
-  s_icon_layer = text_layer_create(GRect(
-      13, PBL_IF_ROUND_ELSE(window_bounds.size.h, window_bounds.size.h - 43),
-      window_bounds.size.w / 2, 60));
+  s_icon_layer =
+      text_layer_create(GRect(13, PBL_IF_ROUND_ELSE(window_bounds.size.h - 43,
+                                                    window_bounds.size.h - 43),
+                              window_bounds.size.w / 2, 60));
 
   // Style the weather icon
   text_layer_set_background_color(s_icon_layer, GColorClear);
@@ -332,7 +315,7 @@ static void window_load(Window *window) {
   text_layer_set_font(s_hour_layer, s_time_font);
   text_layer_set_font(s_minute_layer, s_time_font);
   text_layer_set_font(s_date_layer, s_small_font);
-  text_layer_set_font(s_weather_layer, s_small_font);
+  text_layer_set_font(s_icontext_layer, s_small_font);
   text_layer_set_font(s_icon_layer, s_icon_font);
 
   // Add layers
@@ -343,19 +326,22 @@ static void window_load(Window *window) {
   layer_add_child(window_get_root_layer(window),
                   text_layer_get_layer(s_date_layer));
   layer_add_child(window_get_root_layer(window),
-                  text_layer_get_layer(s_weather_layer));
+                  text_layer_get_layer(s_icontext_layer));
   layer_add_child(window_get_root_layer(window),
                   text_layer_get_layer(s_icon_layer));
 }
 
 static void app_connection_handler(bool connected) {
-  connection_status = connected;
+  if (!connected) {
+    battery_handler(battery_state_service_peek());
+  }
   APP_LOG(APP_LOG_LEVEL_INFO, "Pebble app %sconnected", connected ? "" : "dis");
 }
 
 // static void kit_connection_handler(bool connected) {
 //   connection_status = connected;
-//   APP_LOG(APP_LOG_LEVEL_INFO, "PebbleKit %sconnected", connected ? "" : "dis");
+//   APP_LOG(APP_LOG_LEVEL_INFO, "PebbleKit %sconnected", connected ? "" :
+//   "dis");
 // }
 
 static void window_unload(Window *window) {
@@ -364,7 +350,7 @@ static void window_unload(Window *window) {
   text_layer_destroy(s_hour_layer);
   text_layer_destroy(s_minute_layer);
   text_layer_destroy(s_date_layer);
-  text_layer_destroy(s_weather_layer);
+  text_layer_destroy(s_icontext_layer);
   text_layer_destroy(s_icon_layer);
   fonts_unload_custom_font(s_icon_font);
   fonts_unload_custom_font(s_time_font);
